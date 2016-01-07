d1702c19a47c22ca207a91e4a71a503b1b236eaf
Rasika Gayan <rasikagayangunarathna@gmail.com>
2016-01-08 05:07:29 +0530

    angular is in working state

--- SubtitleProject/App_Start/RouteConfig.cs
+++ SubtitleProject/App_Start/RouteConfig.cs
@@ -18,6 +18,8 @@ namespace SubtitleProject
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+           
         }
     }
 }
---
+++ SubtitleProject/Business_Logic/ReadSubFile.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Web.Mvc;
+
+
+namespace SubtitleProject.Business_Logic
+{
+    public class ReadSubFile
+    {
+
+        //private List<string> subFileContext = null;
+        //public List<string> ReadAll()
+        //{
+        //    System.IO.StreamReader file = new System.IO.StreamReader("c:\\test.txt");
+        //    while ((line = file.ReadLine()) != null)
+        //    {
+        //        Console.WriteLine(line);
+        //        counter++;
+        //    }
+
+        //    file.Close();
+        //}
+
+        public void textMethod()
+        {
+            //string line;
+            //List<string> fileContent = new List<string>();
+            //try
+            //{
+            //    StreamReader file = new StreamReader(Server.MapPath("~/Files/Transporter.srt"));
+            //    while ((line = file.ReadLine()) != null)
+            //    {
+            //        fileContent.Add(line);
+            //    }
+
+            //    file.Close();
+            //}
+            //catch (Exception e)
+            //{
+
+            //}
+        }
+       
+
+    }
+}
