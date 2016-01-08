501de3d8540386ab1b4d9dee4cb32aac6d269ac9
Rasika Gayan <rasikagayangunarathna@gmail.com>
2016-01-08 15:21:06 +0530

    added API config to app start

--- SubtitleProject/Controllers/FileUploadController.cs
+++ SubtitleProject/Controllers/FileUploadController.cs
@@ -49,5 +49,7 @@ namespace SubtitleProject.Controllers
 
 
 
+
+
     }
 }
--- SubtitleProject/Global.asax.cs
+++ SubtitleProject/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace SubtitleProject
 {
@@ -16,6 +17,18 @@ namespace SubtitleProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            
+            GlobalConfiguration.Configure(config =>
+            {
+                config.MapHttpAttributeRoutes();
+
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+            });
+
         }
     }
 }
